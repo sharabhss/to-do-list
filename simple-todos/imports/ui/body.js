4575e70405903864dced341ad430d54de9db71d6
sharabhss <sharabhs93@gmail.com>
2016-04-28 22:29:25 -0400

    Changed body and server side file

---
+++ simple-todos/imports/ui/body.js
@@ -0,0 +1,1 @@
+import { Template } from 'meteor/templating'; import { Tasks } from '../api/tasks.js'; import './body.html'; Template.body.helpers({  tasks() {    return Tasks.find({});  },});
